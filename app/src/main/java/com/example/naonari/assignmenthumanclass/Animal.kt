8d4395ca174fefb1afadd5d1110d8b23dfd401e6
naonari-sakai <sakai.n.w@gmail.com>
2020-08-14 20:46:47 +0900

    Animalクラスにプロパティを追加しました。

--- app/src/main/java/com/example/naonari/assignmenthumanclass/Animal.kt
+++ app/src/main/java/com/example/naonari/assignmenthumanclass/Animal.kt
@@ -1,5 +1,5 @@
 package com.example.naonari.assignmenthumanclass
 
-open abstract class Animal {
+open abstract class Animal(var name : String,var age : Int) {
     abstract fun say()
 }
--- app/src/main/java/com/example/naonari/assignmenthumanclass/Human.kt
+++ app/src/main/java/com/example/naonari/assignmenthumanclass/Human.kt
@@ -2,7 +2,7 @@ package com.example.naonari.assignmenthumanclass
 
 import android.util.Log
 
-class Human (var name: String,var age: Int,var hobby: String): Animal(),Thinkable{
+class Human (name: String, age: Int,var hobby: String ): Animal(name, age),Thinkable{
     override fun say() {
         Log.d("kotlinTest","「名前は" + name + "です。年は"+ age +"歳です。」")
     }
